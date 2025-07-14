b1935f4da14a0e0071c46fa2b8aaf972e92e5272
sheyniklive <sheynik888@yandex.ru>
2025-07-14 17:21:34 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("братищщка, чиркни имя: ");
+        String name = console.nextLine();
+        System.out.println("Теперича - как род ваш именуют сударь: ");
+        String secname = console.nextLine();
+        System.out.print("Твои имя-фамилия, бро: " + name + " " + secname);
+    }
+}
