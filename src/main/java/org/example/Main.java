7d16258b79d22775f8cb316f044c9eb926b0f26d
sheyniklive <sheynik888@yandex.ru>
2025-07-21 17:07:12 +0300

    initiate commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,42 @@
 package org.example;
 
-import org.example.calculator.CalculatorService;
+import java.util.Scanner;
 
 public class Main {
+    static boolean isExit = false;
+    static Scanner console = new Scanner(System.in);
+    static String input;
+
     public static void main(String[] args) {
-        CalculatorService calculator = new CalculatorService();
-        calculator.calculate();
+        System.out.println("Привет, выбери, чем ты хочешь заняться: \n 1 - Имя Фамилия, \n 2 - Калькулятор, \n или введи 'exit' для выхода");
+        input = console.nextLine();
+        while (!isExit) {
+            if (input.equals("1")) {
+
+
+            }
+        }
+
+        private void getChoice () {
+            input = console.nextLine();
+            if (isExit()) {
+                System.exit(0);
+            } else {
+                input = console.nextLine();
+                if (input.equals("1")) {
+
+
+                }
+            }
+
+            private boolean isExit () {
+                if (input.equalsIgnoreCase("exit")) {
+                    System.out.println("Хорошего дня");
+                    return true;
+                }
+                return false;
+            }
+
+
+        }
     }
-}
--- src/main/java/org/example/person/PersonService.java
+++ src/main/java/org/example/person/PersonService.java
@@ -3,13 +3,15 @@ package org.example.person;
 import java.util.Scanner;
 
 public class PersonService {
-public PersonService () {}
-    private static String askImFam() {
+    String name;
+    String secname;
+
+    public String askImFam() {
         Scanner console = new Scanner(System.in);
         System.out.println("братищщка, чекни имя: ");
-        String name = console.nextLine();
+        name = console.nextLine();
         System.out.println("Теперича - как род ваш именуют сударь: ");
-        String secname = console.nextLine();
+        secname = console.nextLine();
         return "Твои имя-фамилия, бро: " + name + " " + secname;
     }
 }
