8690f9d71ebdcd812af196c5d52d4f9ca37eec81
sheyniklive <sheynik888@yandex.ru>
2025-07-22 20:59:48 +0300

    initiate commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,34 @@
 package org.example;
 
+import org.example.calculator.CalculatorService;
+import org.example.person.PersonService;
+
 import java.util.Scanner;
 
 public class Main {
-    static boolean isExit = false;
     static Scanner console = new Scanner(System.in);
     static String input;
 
     public static void main(String[] args) {
         System.out.println("Привет, выбери, чем ты хочешь заняться: \n 1 - Имя Фамилия, \n 2 - Калькулятор, \n или введи 'exit' для выхода");
         input = console.nextLine();
-        while (!isExit) {
-            if (input.equals("1")) {
-
-
-            }
-        }
-
-        private void getChoice () {
+        while (!input.equals("exit") && !input.equals("1") && !input.equals("2")) {
+            System.out.println("только 1, 2 или ''exit");
             input = console.nextLine();
-            if (isExit()) {
+        }
+        switch (input) {
+            case "1":
+                PersonService personService = new PersonService();
+                personService.askImFam();
+                break;
+            case "2":
+                CalculatorService calculatorService = new CalculatorService();
+                calculatorService.calculate();
+                break;
+            case "exit":
+                System.out.println("До новых встреч!");
                 System.exit(0);
-            } else {
-                input = console.nextLine();
-                if (input.equals("1")) {
-
-
-                }
-            }
-
-            private boolean isExit () {
-                if (input.equalsIgnoreCase("exit")) {
-                    System.out.println("Хорошего дня");
-                    return true;
-                }
-                return false;
-            }
-
-
+                break;
         }
     }
+}
