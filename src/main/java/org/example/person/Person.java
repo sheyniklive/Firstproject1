c8da6b119cd0a729b6fedbac4f1ec2fbb5c3abc3
sheyniklive <sheynik888@yandex.ru>
2025-07-23 14:57:38 +0300

    initiate commit

--- src/main/java/org/example/person/Person.java
+++ src/main/java/org/example/person/Person.java
@@ -34,4 +34,9 @@ public class Person {
     public int getAge() {
         return age;
     }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + ", Фамилия: " + surname + ", Возраст: " + age;
+    }
 }
