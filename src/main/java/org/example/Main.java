1f5d8a2d3a39978cebe604bd1bfb2232ba8505f9
sheyniklive <sheynik888@yandex.ru>
2025-07-29 17:03:37 +0300

    add but not clean

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.calculator.CalculatorService;
+import org.example.exits.Exits;
 import org.example.person.PersonService;
 
 import java.util.Scanner;
@@ -10,10 +11,30 @@ public class Main {
     static String input;
     static PersonService personService = new PersonService();
     static CalculatorService calculatorService = new CalculatorService();
+    static Exits exits = new Exits();
 
 
     public static void main(String[] args) {
-        System.out.println("Привет, выбери, чем ты хочешь заняться:");
+        System.out.println("Привет, хочешь перед началом добавить свои варианты выходных реплик?");
+        System.out.println("1 - да,");
+        System.out.println("0 - идем дальше");
+        System.out.println("exit - вообще выйдем");
+        input = console.nextLine();
+        while (!input.equals("1") && !input.equals("0") && !input.equals("exit")) {
+            System.out.println("только 1, 0 или exit");
+            input = console.nextLine();
+        }
+        switch (input) {
+            case "1":
+                exits.addExits();
+                break;
+            case "0":
+                break;
+            case "exit":
+                exits.doExit();
+                break;
+        }
+        System.out.println("хорошо, теперь выбери, чем ты хочешь заняться:");
         System.out.println("1 - Сам(а) введешь свои Имя и Фамилию или выведем сколько-то персонов,");
         System.out.println("2 - Калькулятор,");
         System.out.println("или введи exit для выхода");
@@ -30,8 +51,7 @@ public class Main {
                 calculatorService.calculate();
                 break;
             case "exit":
-                System.out.println("До новых встреч!");
-                System.exit(0);
+                exits.doExit();
                 break;
         }
     }
