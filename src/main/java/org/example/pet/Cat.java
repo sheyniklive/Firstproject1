61ed56b41ca036d8d33556e85f389c6962c848cd
sheyniklive <sheynik888@yandex.ru>
2025-08-01 17:57:34 +0300

    initiate commit

---
+++ src/main/java/org/example/pet/Cat.java
@@ -0,0 +1,22 @@
+package org.example.pet;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Cat implements InterfacePet {
+    private final String name;
+
+
+    @Override
+    public String getType() {
+        return "cat";
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("meow");
+    }
+
+}
--- src/main/java/org/example/pet/InterfacePet.java
+++ src/main/java/org/example/pet/InterfacePet.java
@@ -1,7 +1,14 @@
-package org.example.Pet;
+package org.example.pet;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 public interface InterfacePet {
     String getName();
+
     String getType();
+
     void makeSound();
 }
+
+
