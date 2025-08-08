524efba1e03b38212f7dde6f2e08b740bab86709
sheyniklive <sheynik888@yandex.ru>
2025-08-08 17:09:19 +0300

    initiate commit

--- src/main/java/org/example/exception/InvalidAgeException.java
+++ src/main/java/org/example/exception/InvalidAgeException.java
@@ -1,7 +1,10 @@
 package org.example.exception;
 
 public class InvalidAgeException extends RuntimeException {
-    public InvalidAgeException(String message) {
-        super(message);
+    final int invalidAge;
+
+    public InvalidAgeException(Integer age) {
+        super(String.format("Неверно введен возраст: %d, должно быть от 0 до 150 лет", age));
+        invalidAge = age;
     }
 }
