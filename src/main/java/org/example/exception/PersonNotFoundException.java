112fc02db959cf7f2890e3554faee7d29392a479
sheyniklive <sheynik888@yandex.ru>
2025-08-08 19:00:25 +0300

    initiate commit

--- src/main/java/org/example/exception/PersonNotFoundException.java
+++ src/main/java/org/example/exception/PersonNotFoundException.java
@@ -2,6 +2,7 @@ package org.example.exception;
 
 public class PersonNotFoundException extends RuntimeException {
     final String invalidName;
+
     public PersonNotFoundException(String name) {
         super(String.format("Не найдено человека с именем %s", name));
         invalidName = name;
