bc876255fb4667166b034e287dc5c816ebee435c
sheyniklive <sheynik888@yandex.ru>
2025-09-12 20:25:46 +0300

    initiate commit

--- src/main/java/org/example/repository/PersonRepository.java
+++ src/main/java/org/example/repository/PersonRepository.java
@@ -1,61 +1,68 @@
 package org.example.repository;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.config.DbConfig;
 import org.example.person.Person;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
+@Slf4j
 public class PersonRepository() {
 
-    private static final Logger log = LoggerFactory.getLogger(PersonRepository.class);
     private final DbConfig dbConfig;
 
     public void save(Person person) {
-        String sql = """
-                MERGE INTO persons AS p
-                USING (VALUES (?, ?, ?, ?)) AS v(name, surname, age, pets)
-                ON p.name = v.name
-                WHEN MATCHED THEN
-                    UPDATE SET
-                        surname = v.surname,
-                        age = v.age,
-                        pets = v.pets
-                WHEN NOT MATCHED THEN
-                    INSERT (name, surname, age, pets)
-                    VALUES (v.name,v.surname,v.age,v.pets)
-                """;
-        try (Connection conn = DriverManager.getConnection(dbConfig.getDbUrl(), dbConfig.getUser(), dbConfig.getPassword());
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, person.getName());
-            ps.setString(2, person.getSurname());
-            ps.setInt(3, person.getAge());
-            ps.setString(4, person.getPets());  // подумать как сразу давать после маппера
-            ps.executeUpdate();
+
+        String sqlSelect = "SELECT id, name, surname, age, pets FROM persons WHERE name = ? FOR UPDATE";
+        String sqlUpdate = "UPDATE persons SET surname = ?, age = ?, pets = ? WHERE name = ?";
+        String sqInsert = "INSERT INTO persons (name, surname, age, pets) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(dbConfig.getDbUrl(), dbConfig.getUser(), dbConfig.getPassword())) {
+            conn.setAutoCommit(false);
+            boolean exists;
+
+            try (PreparedStatement ps = conn.prepareStatement(sqlSelect)) {
+                ps.setString(1, person.getName());
+                try (ResultSet rs = ps.executeQuery()) {
+                    exists = rs.next();
+                }
+            }
+            if (exists) {
+                try (PreparedStatement ps = conn.prepareStatement(sqlUpdate)) {
+                    ps.setString(1, person.getSurname());
+                    ps.setInt(2, person.getAge());
+                    ps.setString(3, person.getPets());// подумать с objectmapper`ом
+                    ps.setString(4, person.getName());
+                    ps.executeUpdate();
+                }
+            } else {
+                try (PreparedStatement ps = conn.prepareStatement(sqInsert)) {
+                    ps.setString(1, person.getName());
+                    ps.setString(2, person.getSurname());
+                    ps.setInt(3, person.getAge());
+                    ps.setString(4, person.getPets());  // подумать как сразу давать после маппера
+                    ps.executeUpdate();
+                }
+            }
+            conn.commit();
         } catch (SQLException e) {
             log.error("Ошибка при сохранении персона в БД", e);
+        }
+    }
+
+
+        public Optional<Person> findByName (String name){
+
+        }
+
+
+        public List<Person> findAll () {
+
         }
 
 
     }
-
-
-    public Optional<Person> findByName(String name) {
-
-    }
-
-
-    public List<Person> findAll() {
-
-    }
-
-
-}
