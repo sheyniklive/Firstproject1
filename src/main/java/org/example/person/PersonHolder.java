72e8f9723ba97e2786015bb39e9c14bfa270e793
sheyniklive <sheynik888@yandex.ru>
2025-09-22 23:31:56 +0300

    initiate commit

--- src/main/java/org/example/person/PersonHolder.java
+++
@@ -1,9 +0,0 @@
-package org.example.person;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class PersonHolder {
-
-    public static final Map<String, Person> personHolder = new HashMap<>();
-}
