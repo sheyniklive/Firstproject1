ed6fba0c3f7ee39d0d2168cf262d03c3d2336c2e
sheyniklive <sheynik888@yandex.ru>
2025-10-27 19:17:25 +0300

    add lil in save (PersonRepositoryV2)

--- src/main/java/org/example/repository/PersonRepositoryV2.java
+++ src/main/java/org/example/repository/PersonRepositoryV2.java
@@ -1,10 +1,13 @@
 package org.example.repository;
 
+import org.example.entity.PersonEntity;
 import org.example.person.Person;
+import org.example.person.PersonApiMapper;
 
 public class PersonRepositoryV2 {
 
     public void save(Person person) {
+        PersonEntity entity = PersonApiMapper.toEntityFromDomain(person);
 
     }
 
