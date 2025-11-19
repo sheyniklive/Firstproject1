a315d2f0ff2172d1c9ad9f47446c49220044c56f
sheyniklive <sheynik888@yandex.ru>
2025-11-19 15:27:30 +0300

    add Exception

--- src/main/java/org/example/exception/InvalidOwnershipException.java
+++ src/main/java/org/example/exception/InvalidOwnershipException.java
@@ -6,4 +6,5 @@ public class InvalidOwnershipException extends RuntimeException {
     public InvalidOwnershipException(Long petId, UUID personId) {
         super(String.format("Pet with id {%s} does not belong to person with id {%s}", petId, personId));
     }
+
 }
--- src/main/java/org/example/exception/PetNotFoundException.java
+++ src/main/java/org/example/exception/PetNotFoundException.java
@@ -4,4 +4,5 @@ public class PetNotFoundException extends RuntimeException {
     public PetNotFoundException(Long id) {
         super(String.format("Pet with id %d not found", id));
     }
+
 }
