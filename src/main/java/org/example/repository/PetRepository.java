ac3cca3a532b6353d049a2630e8c90f8c4365e35
sheyniklive <sheynik888@yandex.ru>
2025-12-09 15:11:25 +0300

    lil fix in PetRepository

--- src/main/java/org/example/repository/PetRepository.java
+++ src/main/java/org/example/repository/PetRepository.java
@@ -2,6 +2,9 @@ package org.example.repository;
 
 import org.example.entity.PetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +15,13 @@ public interface PetRepository extends JpaRepository<PetEntity, Long> {
 
     List<PetEntity> findPetsByOwnerId(UUID ownerId);
 
-    int deleteAllPetsByOwnerId(UUID ownerId);
+    @Modifying
+    @Query("DELETE FROM PetEntity p WHERE p.owner.id = :ownerId")
+    int deleteAllPetsByOwnerId(@Param("ownerId") UUID ownerId);
 
-    int deletePetByOwnerIdAndId(UUID ownerId, Long id);
+    @Modifying
+    @Query("DELETE FROM PetEntity p WHERE p.owner.id = :ownerId AND p.id = :id")
+    int deletePetByOwnerIdAndId(@Param("ownerId") UUID ownerId, @Param("id") Long id);
 
     boolean existsByOwnerIdAndId(UUID ownerId, Long id);
 }
