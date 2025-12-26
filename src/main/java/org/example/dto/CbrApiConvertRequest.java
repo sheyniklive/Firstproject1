7f3fd5c930de9dd2f5324fd0730487355827d99b
sheyniklive <sheynik888@yandex.ru>
2025-12-26 19:20:26 +0300

    add validation dependency lil add annotation (raw)

--- src/main/java/org/example/dto/CbrApiConvertRequest.java
+++ src/main/java/org/example/dto/CbrApiConvertRequest.java
@@ -1,13 +1,24 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Data
+@Validated
 public class CbrApiConvertRequest {
+
+    @NotBlank(message = "Сумма не может быть пустой")//определиться
+    @DecimalMin(value = "0.01", message = "Сумма должна быть больше 0")
     private BigDecimal amountInRubles;
+
+    @NotBlank(message = "Код валюты не может быть пустым")//тоже
+    @Size(min = 3, max = 3, message = "Код валюты должен состоять из 3-х букв")
     private String targetCurrency;
 }
