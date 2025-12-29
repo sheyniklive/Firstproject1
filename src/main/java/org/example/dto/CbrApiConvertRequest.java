ca79673dc1d3f07e597a7a19887db806b1250129
sheyniklive <sheynik888@yandex.ru>
2025-12-29 14:25:01 +0300

    remove self-comments

--- src/main/java/org/example/dto/CbrApiConvertRequest.java
+++ src/main/java/org/example/dto/CbrApiConvertRequest.java
@@ -13,11 +13,11 @@ import java.math.BigDecimal;
 @Data
 public class CbrApiConvertRequest {
 
-    @NotNull(message = "Сумма не может быть пустой")//определиться
+    @NotNull(message = "Сумма не может быть пустой")
     @DecimalMin(value = "0.0", inclusive = false, message = "Сумма должна быть больше 0")
     private BigDecimal amountInRubles;
 
-    @NotBlank(message = "Код валюты не может быть пустым")//тоже
+    @NotBlank(message = "Код валюты не может быть пустым")
     @Size(min = 3, max = 3, message = "Код валюты должен состоять из 3-х букв")
     private String targetCurrency;
 }
