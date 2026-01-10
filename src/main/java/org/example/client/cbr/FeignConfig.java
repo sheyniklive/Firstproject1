005483edec9878705ca3d1e2c3d28a76deaff01c
sheyniklive <sheynik888@yandex.ru>
2026-01-10 16:08:39 +0300

    add decoder javascript -> java

--- src/main/java/org/example/client/cbr/FeignConfig.java
+++ src/main/java/org/example/client/cbr/FeignConfig.java
@@ -1,10 +1,31 @@
 package org.example.client.cbr;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Retryer;
+import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.List;
 
 public class FeignConfig {
+
+    @Bean
+    public Decoder feignDecoder(ObjectMapper objectMapper) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(objectMapper);
+        converter.setSupportedMediaTypes(List.of(
+                MediaType.APPLICATION_JSON,
+                new MediaType("application", "javascript")
+        ));
+        ObjectFactory<HttpMessageConverters> factory = () -> new HttpMessageConverters(converter);
+        return new ResponseEntityDecoder(new SpringDecoder(factory));
+    }
 
     @Bean
     public Retryer retryer() {
