847a0464e7d6ce611eb1a98b455c2a973e13c722
sheyniklive <sheynik888@yandex.ru>
2026-07-14 17:32:40 +0300

    start PetServiceTest

---
+++ src/test/java/org/example/services/PetServiceTest.java
@@ -0,0 +1,32 @@
+package org.example.services;
+
+import org.example.pet.Pet;
+import org.example.repository.PersonRepository;
+import org.example.repository.PetRepository;
+import org.example.service.PetService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class PetServiceTest {
+
+    @Mock
+    private PersonRepository  personRepo;
+
+    @Mock
+    private PetRepository petRepo;
+
+    @InjectMocks
+    private PetService petService;
+
+    @Captor
+    ArgumentCaptor<List<Pet>> captor;
+
+
+}
